37bffe1fd08027d96bb7f1d98d6ac4e42fab18d9
alexpchin <alex@alexchin.co.uk>
2016-08-01 20:05:41 +0100

    Adding the map

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,9 +7,7 @@ function initMap() {
     zoom: 8,
     center: {lat: 53.1280137, lng: -1.5884946},
     scrollwheel:  false,
-    draggable: false,
-    disableDoubleClickZoom: true,
-    panControl: false,
+    draggable: true,
     streetViewControl: false,
   });
   directionsDisplay.setMap(map);
